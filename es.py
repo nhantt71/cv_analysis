9d29e154613e06dcc7cdf156b96dea3dcc89c302
To Trong Nhan <125555189+nhantt71@users.noreply.github.com>
2025-04-17 02:58:51 +0700

    Update es.py

--- es.py
+++ es.py
@@ -1,12 +1,8 @@
 from elasticsearch import Elasticsearch
 
 es = Elasticsearch(
-    "https://open-university-sear-4132308520.ap-southeast-2.bonsaisearch.net:443",
-    basic_auth=("3pwadlnjvm", "e82nqrvykx"),
-    headers={"Content-Type": "application/json"}
+    hosts=["https://open-university-sear-4132308520.ap-southeast-2.bonsaisearch.net"],
+    http_auth=("3pwadlnjvm", "e82nqrvykx"),  # dùng http_auth cho elasticsearch 7.x
 )
-
-def index_cv_data(data: dict, index: str = "cv_index"):
-    es.index(index=index, body=data)
 
 
