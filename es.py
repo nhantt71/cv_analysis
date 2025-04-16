bc7d81dd0292a17d031920428e3209c6ec27ae1f
To Trong Nhan <125555189+nhantt71@users.noreply.github.com>
2025-04-17 03:22:27 +0700

    Update es.py

--- es.py
+++ es.py
@@ -2,7 +2,9 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch(
     "https://open-university-sear-4132308520.ap-southeast-2.bonsaisearch.net",
-    http_auth=("3pwadlnjvm", "e82nqrvykx")
+    http_auth=("3pwadlnjvm", "e82nqrvykx"),
+    verify_certs=True,
+    ssl_show_warn=False
 )
 
 # Kiểm tra kết nối
