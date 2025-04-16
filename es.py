db260cc1bb2566519e305abc44d303b2e1ca4160
To Trong Nhan <125555189+nhantt71@users.noreply.github.com>
2025-04-17 00:47:47 +0700

    es modified

--- es.py
+++ es.py
@@ -2,7 +2,8 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch(
     "https://open-university-sear-4132308520.ap-southeast-2.bonsaisearch.net:443",
-    http_auth=("3pwadlnjvm", "e82nqrvykx")
+    basic_auth=("3pwadlnjvm", "e82nqrvykx"),
+    headers={"Content-Type": "application/json"}
 )
 
 def index_cv_data(data: dict, index: str = "cv_index"):
